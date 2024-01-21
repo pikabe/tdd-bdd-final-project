830d7d9a68ccac91ff4fd40cd651f6e309260ed5
pikabe <35191200+pikabe@users.noreply.github.com>
2024-01-21 13:16:47 +0000

    Update web_steps.py  Implementing Steps

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -106,6 +106,7 @@ def step_impl(context, element_name):
 
 ## UPDATE CODE HERE ##
 
+
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
@@ -113,6 +114,7 @@ def step_impl(context, element_name):
 # We can then lowercase the name and prefix with pet_ to get the id
 ##################################################################
 
+    
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
     element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
@@ -132,3 +134,33 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+    
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
